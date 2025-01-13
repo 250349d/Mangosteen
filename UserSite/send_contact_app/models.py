455434864beace41d8950b4bafc83dadc8eb3309
250349d <root@UBUNTU.>
2025-01-13 17:50:54 +0900

    add U

--- UserSite/send_contact_app/models.py
+++ UserSite/send_contact_app/models.py
@@ -3,9 +3,12 @@ from django.contrib import auth
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.core.mail import send_mail
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import PermissionsMixin
+
+CustomUser = get_user_model()
 
 def get_sentinel_user():
     return CustomUser.objects.get_or_create(
